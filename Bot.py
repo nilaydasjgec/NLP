ae00bc7b00ebb501101cc48318ceb921457a720b
NILAY <nilay.das.jgec@gmail.com>
2019-05-26 22:50:57 +0530

    Initial commit

---
+++ Bot.py
@@ -0,0 +1,11 @@
+from nltk import pos_tag
+from nltk import RegexpParser
+text ="learn php from guru99 and make study easy".split()
+print("After Split:",text)
+tokens_tag = pos_tag(text)
+print("After Token:",tokens_tag)
+patterns= """mychunk:{<NN.?>*<VBD.?>*<JJ.?>*<CC>?}"""
+chunker = RegexpParser(patterns)
+print("After Regex:",chunker)
+output = chunker.parse(tokens_tag)
+print("After Chunking",output)
